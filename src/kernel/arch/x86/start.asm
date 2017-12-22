b3161db7f61db5d8df62ade5003c7853f0d89d17
Atul <kkmonlee@gmail.com>
2017-12-22 00:54:32 +0000

    Created bootsequence

---
+++ src/kernel/arch/x86/start.asm
@@ -0,0 +1,47 @@
+global _start, _kmain
+extern kmain, start_ctors, end_ctors, start_dtors, end_dtors
+
+%define MULTIBOOT_HEADER_MAGIC	0x1BADB002
+%define MULTIBOOT_HEADER_FLAGS	0x00000003
+%define CHECKSUM -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+; Entry point
+_start:
+	jmp start
+
+; Multiboot header
+align 4
+
+multiboot_header:
+dd MULTIBOOT_HEADER_MAGIC
+dd MULTIBOOT_HEADER_FLAGS
+dd CHECKSUM
+
+start:
+	push ebx
+
+static_ctors_loop:
+	mov ebx, start_ctors
+	jmp .test
+.body:
+	call [ebx]
+	add ebx,4
+.test:
+	cmp ebx, end_ctors
+	jb .body
+	
+	; Call kernel
+	call kmain
+
+static_dtors_loop:
+	mov ebx, start_dtors
+	jmp .text
+.body:
+	call [ebx]
+	add ebx,4
+.test:
+	cmp ebx, end_dtors
+	jb .body
+	
+	cli ; stop interrupts
+	hlt ; halt cpu
