ce84c295c97e944f624fa1d0c5328f1f72dd0fe5
kkmonlee <kkmonlee@gmail.com>
2024-12-14 00:39:49 +0000

    add socket class for managing sockets in kernel core

---
+++ src/kernel/core/socket.h
@@ -0,0 +1,25 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <core/file.h>
+#include <runtime/list.h>
+
+class Socket : public File
+{
+public:
+  Socket(const char *name);
+  virtual ~Socket();
+
+  u32 open(u32 flag) override;
+  u32 close() override;
+  u32 read(u8 *buffer, u32 size) override;
+  u32 write(const u8 *buffer, u32 size) override;
+  u32 ioctl(u32 id, u8 *buffer) override;
+  u32 remove() override;
+  void scan() override;
+
+protected:
+  // TODO: maybe socket name? or socket type?
+};
+
+#endif // SOCKET_H
